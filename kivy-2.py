b88da47f4d6d07e99b42728fc9fa037d207d2b7d
Jim Lynch <jim@jam.sessionsnet.org>
2017-06-10 04:00:25 -0700

    add the username TextInput widget (new import) to the LoginScreen's __init__ method

--- kivy-2.py
+++ kivy-2.py
@@ -6,12 +6,15 @@ kivy.require("1.8.0")
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
 
 class LoginScreen(GridLayout):
     def __init__(self, **kwargs):
         super(LoginScreen, self).__init__(kwargs)
         self.cols = 2
         self.add_widget(Label(text="Username:"))
+        self.username = TextInput(multiline=False)
+        self.add_widget(username)
 
 class SimpleKivy(App):
     def build(self):
