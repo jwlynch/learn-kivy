761d1ce42a760962ca19a4cc1da6ff96ba55380c
Jim Lynch <jim@jam.sessionsnet.org>
2017-06-10 02:24:17 -0700

    second foray into kivy (right now just a copy of the first one)

---
+++ kivy-2.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python3
+
+import kivy
+kivy.require("1.8.0")
+
+from kivy.app import App
+from kivy.uix.label import Label
+
+class SimpleKivy(App):
+    def build(self):
+        return Label(text="hi")
+
+if __name__ == "__main__":
+    # don't forget () after SimpleKivy
+    # (this creates an instance
+    # of the SimpleKivy class)
+    SimpleKivy().run()
