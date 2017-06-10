6408572533959013e47f6897f8d716d5ca3fc750
Jim Lynch <jim@jam.sessionsnet.org>
2017-06-09 22:51:51 -0700

    run the app (def'd by our SimpleKivy app class) if not imported

--- first-kivy.py
+++ first-kivy.py
@@ -8,3 +8,9 @@ from kivy.uix.label import Label
 
 class SimpleKivy(App):
     pass
+
+if __name__ == "__main__":
+    # don't forget () after SimpleKivy
+    # (this creates an instance
+    # of the SimpleKivy class)
+    SimpleKivy().run()
